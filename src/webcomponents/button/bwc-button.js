47249d5060fff9081560e9c4c7e9f798f35c1a56
Dylan Maxey <devbydylan@gmail.com>
2024-04-25 10:52:36 -0400

    importable update

---
+++ src/webcomponents/button/bwc-button.js
@@ -0,0 +1,40 @@
+import './bmd-button.js'
+import { LitElement } from 'lit'
+import { html, unsafeStatic } from 'lit/static-html.js';
+import propSpecs from './props.js'
+
+class BeelineButton extends LitElement {
+  static properties = propSpecs
+
+  constructor() {
+    super()
+    
+    // set defaults to be overridden
+    for (const prop in propSpecs) {
+        if (propSpecs[prop].default) {
+            console.log('defaultprop', prop, propSpecs[prop].default)
+            this[prop] = propSpecs[prop].default
+        }
+    }
+  }
+
+  render() {
+    const buttonTag = unsafeStatic(`bmd-${ this.variant }-button`);
+    window.test = this
+    console.log('render', this)
+
+    return html`
+        <${buttonTag}
+        id="${this.id}"
+        class="btn--${this.size} btn--${this.type}"
+        ?disabled="${this.disabled}"
+        .trailingIcon="${this.icon && this.iconPosition === 'trailing'}"
+        >
+        ${this.icon ? html`<md-icon slot="icon">${this.icon}</md-icon>` : ''}
+        <slot>${this.label}</slot>
+        </${buttonTag}>
+    `;
+  }
+}
+
+customElements.define('bwc-button', BeelineButton)
